a1290241987d1abbc2c29381886e32c4c0818b15
frowningfrog <natalizedstolpinator@hotmail.com>
2026-04-14 20:42:14 -0600

    api works

--- reactfinal/src/components/MonList.jsx
+++ reactfinal/src/components/MonList.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { MonCard } from "./MonCard";
 
 export function DisplayMons({ mons }) {
+  const listData = mons.data;
+  let pokemon = [];
+  if (listData && listData.pokemon) {
+    pokemon = listData.pokemon;
+  }
+  console.log(pokemon[0]);
   return (
     <ul className="list">
-      {mons && mons.map((m) => <MonCard key={m.name} mon={m} />)}
+      {pokemon && pokemon.map((m) => <MonCard key={m.name} mon={m} />)}
     </ul>
   );
 }
--- reactfinal/src/hooks/hooks.jsx
+++ reactfinal/src/hooks/hooks.jsx
@@ -1,29 +1,42 @@
 import { useState, useEffect } from "react";
 
-export function usePokeList(searchTerm) {
-  const [data, setData] = useState(null);
+const getstuff = `
+query samplePokeAPIquery {
+  pokemon(limit: 151) {
+    name
+    pokemontypes {
+      type {
+        name
+      }
+    }
+    pokemonsprites {
+      sprites(path: "front_default")
+    }
+  }
+}`;
+
+export function usePokeList() {
+  const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(
-          `https://pokeapi.co/api/v2/pokemon?limit=1350`,
-        );
+        const response = await fetch(`https://graphql.pokeapi.co/v1beta2`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            query: getstuff,
+          }),
+        });
         if (!response.ok) {
           throw new Error(`HTTP error: Status ${response.status}`);
         }
         const result = await response.json();
-        const seenDex = [];
-        const uniqueMons = result.results.filter((mon) => {
-          if (seenDex.includes(mon.name)) {
-            return false;
-          }
-          seenDex.push(mon.name);
-          return true;
-        });
-        setData(uniqueMons);
+        setData(result);
         setError(null);
       } catch (err) {
         setError(err.message);
@@ -34,9 +47,7 @@ export function usePokeList(searchTerm) {
     };
 
     fetchData();
-  }, [searchTerm]);
-
-  console.log(data);
+  }, []);
 
   return { data, isLoading, error };
 }
