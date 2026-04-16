e23f8abd8953aa210d3aac7ff56ac86be0f7a40f
frowningfrog <natalizedstolpinator@hotmail.com>
2026-04-16 12:25:48 -0600

    type colors

--- reactfinal/src/components/MonCard.jsx
+++ reactfinal/src/components/MonCard.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 
 export function MonCard({ mon }) {
+  const type = mon.pokemontypes[0].type.name;
+  let color = "";
+  switch (type) {
+    case "grass":
+      color = "lightgreen";
+      break;
+    case "fire":
+      color = "red";
+      break;
+  }
+  console.log(type);
   return (
-    <div className="card">
+    <div className="card" style={{ backgroundColor: color }}>
       <img src={mon.pokemonsprites[0].sprites} />
       <p>{mon.name[0].toUpperCase() + mon.name.slice(1)}</p>
     </div>
