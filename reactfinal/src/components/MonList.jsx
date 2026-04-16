c73af8f1a3d75f38e8c1c5cd66f7c18ac4c95c42
frowningfrog <natalizedstolpinator@hotmail.com>
2026-04-16 16:19:29 -0600

    add searchbar

--- reactfinal/src/components/MonList.jsx
+++ reactfinal/src/components/MonList.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { MonCard } from "./MonCard";
 
 export function DisplayMons({ mons }) {
+  const [searchTerm, setSearchTerm] = useState("");
   const listData = mons.data;
   let pokemon = [];
   if (listData && listData.pokemon) {
     pokemon = listData.pokemon;
   }
+  function handleChange(event) {
+    setSearchTerm(event.target.value);
+  }
+
   return (
-    <div className="list">
-      {pokemon && pokemon.map((m) => <MonCard key={m.name} mon={m} />)}
+    <div>
+      <input value={searchTerm} onChange={handleChange} />
+
+      <div className="list">
+        {pokemon &&
+          pokemon
+            .filter((m) => m.name.includes(searchTerm))
+            .map((m) => <MonCard key={m.name} mon={m} />)}
+      </div>
     </div>
   );
 }
